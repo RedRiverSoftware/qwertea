f6db8c819493a9b6922a8998185a0363c5721321
William Venner <william@venner.io>
2016-06-22 11:59:28 -0400

    MySQL in the bot and installation progress

--- slack/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	token: "xoxb-53192437588-oc39QZSP9ggWLIv1IPKfmX6T",
-}
-
-// procrastinator : xoxb-53025046338-0jlW49aXLflVMsKPRwehSHoR
-// rrsoftware     : xoxb-53192437588-oc39QZSP9ggWLIv1IPKfmX6T
